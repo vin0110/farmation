f16c9058feb1ecf74e5aa3c76a056a847a9bfe92
VW Freeh <vwfreeh@gmail.com>
2019-08-02 00:22:14 -0400

    completed add/remove crop to scenario

--- optimizer/forms.py
+++ optimizer/forms.py
@@ -13,11 +13,12 @@ class ScenarioEditForm(forms.ModelForm):
 CROP_CHOICES = [(n, n, ) for n in settings.CROPS]
 
 
-class CropAddForm(forms.Form):
-    name = forms.ChoiceField(choices=CROP_CHOICES)
-
-
 class CropAcresSetForm(forms.Form):
     low = forms.IntegerField(min_value=0)
     high = forms.IntegerField(min_value=0,
                               help_text="0 means unlimited")
+
+
+class AddCropForm(forms.Form):
+    '''select a crop. the list is dynamic; set none here; view will expand'''
+    crop = forms.ChoiceField(choices=CROP_CHOICES)
