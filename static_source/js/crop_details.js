4d7ef03b87cb950ef7c0186df8aca28d4e1c58ab
demills <demills@ncsu.edu>
2019-08-12 09:45:32 -0400

    Deleted crop_details.js for now.

--- static_source/js/crop_details.js
+++
@@ -1,9 +0,0 @@
-var chartA = c3.generate({
-    bindto: '#chartA',
-    data: {
-      columns: [
-        ['data1', 10, 20, 10, 30, 50, 40],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]
-    }
-});
