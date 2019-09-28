92b0e0b685088a84e512e7e8e9e676ac406fdb5e
demills <demills@ncsu.edu>
2019-09-28 01:37:38 -0400

    Updated API to return individual crops and ...
    ... lists of crops associated with a scenario.

--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from optimizer.models import (CropData,
 from .serializers import (CropDataSerializer,
                           ScenarioListSerializer,
                           ScenarioDetailSerializer, 
+                          CropSerializer,
                           )
 
 
@@ -49,4 +50,20 @@ class ScenarioCropsList(generics.GenericAPIView):
         scenario = Scenario.objects.get(pk=pk)
         scenario_crops = scenario.crops.all()
         data = { 'cropnames': [ c.data.name for c in scenario_crops ] }
+        print( data )
         return JsonResponse(data)
+
+
+class CropTriangles(generics.RetrieveAPIView):
+    '''Return cropdata by name'''
+    serializer_class = CropSerializer
+    queryset = Crop.objects.all()
+
+
+class CropList(generics.ListAPIView):
+    '''list scenarios by farm'''
+    serializer_class = CropSerializer
+
+    def get_queryset(self):
+        return Crop.objects.filter(scenario_id=self.kwargs['pk'])
+
