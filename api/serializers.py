7d0dd39ad3f07b2344dad2e5404c636288abfa7c
demills <demills@ncsu.edu>
2019-10-09 18:55:40 -0400

    Fixed the CropSeralizer to return CropData ...
    ... prices and yields instead of Crop's.

--- api/serializers.py
+++ api/serializers.py
@@ -8,30 +8,30 @@ class CropDataSerializer(serializers.ModelSerializer):
     '''serializes CropData'''
     class Meta:
         model = CropData
-        fields = ('name', 'unit', 'price_histo')
+        fields = ('name', 'unit', 'prices', 'yields', )
 
 class CropSerializer(serializers.ModelSerializer):
     name = serializers.SerializerMethodField()
     unit = serializers.SerializerMethodField()
+    cost = serializers.SerializerMethodField()
 
     price_triangle = serializers.SerializerMethodField()
     yield_triangle = serializers.SerializerMethodField()
-    gross_triangle = serializers.SerializerMethodField()
 
     def get_yield_triangle(self, crop):
-        return json.loads(crop.yields())
+        return json.loads(crop.data.yields)
     def get_price_triangle(self, crop):
-        return json.loads(crop.prices())
-    def get_gross_triangle(self, crop):
-        return crop.gross()
+        return json.loads(crop.data.prices)
     def get_name(self, crop):
         return crop.data.name
     def get_unit(self, crop):
         return crop.data.unit
+    def get_cost(self, crop):
+        return crop.data.cost
 
     class Meta:
         model = Crop
-        fields = ('name', 'unit', 'price_triangle', 'yield_triangle', 'gross_triangle')
+        fields = ('name', 'unit', 'price_triangle', 'yield_triangle', 'cost')
 
 class ScenarioListSerializer(serializers.ModelSerializer):
     '''serializes scenarios for a farm'''
